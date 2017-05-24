df6ae0c9b9c8975afeb0a94f53d749fac0c91f30
Alex Nichol <unixpickle@gmail.com>
2017-05-24 12:17:35 -0400

    started designing download system

---
+++ games/injections/famobi.js
@@ -0,0 +1,21 @@
+var _fgq = [];
+
+function fg_api(data) {
+  this._data = data;
+}
+
+fg_api.prototype.__ = function(key) {
+  var res = this._data.game_i18n.en[key];
+  return res.replace('{lang}', 'en');
+}
+
+fg_api.prototype.getMoreGamesButtonImage = function() {
+  return '';
+};
+
+fg_api.prototype.moreGamesLink = function() {
+  return '';
+};
+
+fg_api.prototype.submitHighscore = function() {
+};
