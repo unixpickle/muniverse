ccaa729a81f3b4c697fb850596cb8f59d5fd26b5
Alex Nichol <unixpickle@gmail.com>
2017-05-24 17:46:36 -0400

    first draft of injection for KatanaFruits-v0

--- games/injections/KatanaFruits-v0.js
+++ games/injections/KatanaFruits-v0.js
@@ -1,16 +1,6 @@
 (function() {
 
-  window.muniverse = {
-    score: function() {
-      return 0;
-    },
-    reset: function() {
-      // TODO: navigate the menus and get to a game.
-    },
-    step: function() {
-      // TODO: advance time here.
-    }
-  };
+  var MENU_TIMEOUT = 10000;
 
   var oldCInterface = window.CInterface;
   window.CInterface = function() {
@@ -24,5 +14,57 @@
       };
     }
   }
+
+  var oldCHelpPanel = window.CHelpPanel;
+  var globalCHelpPanel = null;
+  window.CHelpPanel = function() {
+    oldCHelpPanel.call(this);
+    globalCHelpPanel = this;
+  };
+
+  function pollAndWait(timeout, check) {
+    var INTERVAL = 100;
+    return new Promise(function(resolve, reject) {
+      var ival;
+      ival = setInterval(function() {
+        if (check()) {
+          clearInterval(ival);
+          resolve();
+        }
+        timeout -= INTERVAL;
+        if (timeout < 0) {
+          clearInterval(ival);
+          reject('timeout surpassed');
+        }
+      }, INTERVAL);
+    });
+  }
+
+  window.muniverse = {
+    init: function() {
+      return pollAndWait(MENU_TIMEOUT, function() {
+        return window.hasOwnProperty('s_oMenu');
+      }).then(function() {
+        window.s_oMenu._onButPlayRelease();
+        return pollAndWait(MENU_TIMEOUT, function() {
+          return globalCHelpPanel !== null;
+        });
+      }).then(function() {
+        globalCHelpPanel._onExit();
+        window.faketime.pause();
+      });
+    },
+
+    score: function() {
+      return 0;
+    },
+
+    step: function(millis) {
+      return new Promise(function(resolve) {
+        window.faketime.advance(millis);
+        resolve();
+      });
+    }
+  };
 
 })();
