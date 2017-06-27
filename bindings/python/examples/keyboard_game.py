83190e155ad77eef03e12fe4b6cc9fbbb5a2c6c9
Alex Nichol <unixpickle@gmail.com>
2017-06-26 21:22:55 -0400

    added Python keyboard game example

---
+++ bindings/python/examples/keyboard_game.py
@@ -0,0 +1,32 @@
+"""
+Simple program to demonstrate how to use muniverse.
+"""
+
+import sys
+sys.path.insert(0, '..')
+
+import muniverse
+
+def main():
+    print('Looking up environment...')
+    spec = muniverse.spec_for_name('MinimalDots-v0')
+    print('Creating environment...')
+    env = muniverse.Env(spec)
+    try:
+        print('Resetting environment...')
+        env.reset()
+
+        print('Playing game...')
+        action = muniverse.key_for_code('ArrowRight')
+        while True:
+            reward, done = env.step(0.1, action.with_event('keyDown'),
+                                    action.with_event('keyUp'))
+            print('reward: ' + str(reward))
+            if done:
+                break
+
+    finally:
+        env.close()
+
+if __name__ == '__main__':
+    main()
