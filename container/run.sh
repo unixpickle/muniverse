60a769b5919c6907047957189a8a5210eb7cdef2
Alex Nichol <unixpickle@gmail.com>
2018-10-16 13:19:30 -0400

    use chromium from debian package
    Fixes #4. Fixes WebGL problems on BubblesShooter-v0

--- container/run.sh
+++ container/run.sh
@@ -2,14 +2,13 @@
 
 fsserver -path=/downloaded_games &
 
-/chrome-linux/chrome \
+chromium-browser \
   --headless \
   --mute-audio \
   --no-sandbox \
   --remote-debugging-port=9222 \
   --remote-debugging-address=0.0.0.0 \
   --disable-gpu \
-  --disable-webgl \
   "$@" &
 
 # Use a socket close to trigger termination.
